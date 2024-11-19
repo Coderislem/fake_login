1f8e7cd320b0fa95065c271d78abafc2c72c7bbe
islam beghour <https://github.com/Coderislem>
2024-11-19 21:36:17 +0100

    modify settings

--- fake_page/settings.py
+++ fake_page/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b12n+4i8rjajk*_(7fp6v5er$3hq%f7@g5ipy2_1-!ia82y9aw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR /"staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
